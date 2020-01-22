5db597aacb0ff430a49f56196a3176d9d981c9c0
Abhishek Kohli <help143341@gmail.com>
2020-01-22 16:53:37 +0530

    new code

---
+++ Duplicate/Duplicate/Duplicate.h
@@ -0,0 +1,19 @@
+//
+//  Duplicate.h
+//  Duplicate
+//
+//  Created by Apple on 22/01/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Duplicate.
+FOUNDATION_EXPORT double DuplicateVersionNumber;
+
+//! Project version string for Duplicate.
+FOUNDATION_EXPORT const unsigned char DuplicateVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Duplicate/PublicHeader.h>
+
+
---
+++ Duplicate/Duplicate/Services.swift
@@ -0,0 +1,19 @@
+//
+//  Services.swift
+//  Duplicate
+//
+//  Created by Apple on 22/01/20.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import Foundation
+
+public class Services {
+    
+    public init(){}
+    
+    public static func doSomething() -> String {
+        return "YO"
+    }
+}
+ 
